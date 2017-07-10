c7e93d3460d78b088372d868389395082e2066e0
Smiller193 <shawn.miller@temple.edu>
2017-07-10 15:04:42 -0400

    First commit

--- Makestagram/AppDelegate.swift
+++ Makestagram/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -39,6 +40,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    
+    
+    class AppDelegate: UIResponder, UIApplicationDelegate {
+        
+        var window: UIWindow?
+        
+        func application(_ application: UIApplication,
+                         didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?)
+            -> Bool {
+                FirebaseApp.configure()
+                return true
+        }
     }
 
 
---
+++ Makestagram/Views/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  Makestagram
+//
+//  Created by Shawn Miller on 7/10/17.
+//  Copyright © 2017 Make School. All rights reserved.
+//
+
+import Foundation
+
+
+class LoginViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
