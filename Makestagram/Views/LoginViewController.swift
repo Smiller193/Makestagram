cccbee2c84b102408902623cf4b7d35dabd48e19
Smiller193 <shawn.miller@temple.edu>
2017-07-10 22:38:35 -0400

    Makestagram Label and tag label constraints properly created

--- Makestagram/Views/LoginViewController.swift
+++ Makestagram/Views/LoginViewController.swift
@@ -7,17 +7,12 @@
 //
 
 import Foundation
+import UIKit
 
 
-class LoginViewController: ViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }
