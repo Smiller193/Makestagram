53dc585aac720cf6393217e6165ea3bc0582bfd1
Smiller193 <shawn.miller@temple.edu>
2017-07-11 11:21:19 -0400

    Management of user accounts improved

--- Makestagram/Views/LoginViewController.swift
+++ Makestagram/Views/LoginViewController.swift
@@ -8,9 +8,14 @@
 
 import Foundation
 import UIKit
+import FirebaseAuth
+import FirebaseAuthUI
+import FirebaseDatabase
 
 
-class LoginViewController: UIViewController {
+typealias FIRUser = FirebaseAuth.User
+
+class LoginViewController: UIViewController, FUIAuthDelegate {
 
     // MARK: - Properties
     @IBOutlet weak var loginButton: UIButton!
@@ -21,7 +26,41 @@ class LoginViewController: UIViewController {
     // MARK: - IBActions
     @IBAction func loginButtonTapped(_ sender: Any) {
         print("Login button tapped")
+        // 1
+        guard let authUI = FUIAuth.defaultAuthUI()
+            else { return }
+        // 2
+        authUI.delegate = self
+        // 3
+        let authViewController = authUI.authViewController()
+        present(authViewController, animated: true)
     }
     
+    func authUI(_ authUI: FUIAuth, didSignInWith user: FIRUser?, error: Error?) {
+        if let error = error {
+            assertionFailure("Error signing in: \(error.localizedDescription)")
+            return
+        }
+        
+        
+        guard let user = user
+            else {return}
+        
+        let userRef = Database.database().reference().child("users").child(user.uid)
+        userRef.observeSingleEvent(of: .value, with: {(snapshot) in
+        // 4 retrieve user data from snapshot
+        
+            if let user = User(snapshot: snapshot) {
+                print("Welcome back, \(user.username).")
+            } else {
+                print("New user!")
+            }
+        })
+        
+        print("handle user signup / login")
+    }
     
 }
+
+
+
