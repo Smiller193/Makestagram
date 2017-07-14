74818800d09f2020d720670585a703896a22dc33
Smiller193 <shawn.miller@temple.edu>
2017-07-14 10:57:08 -0400

    Implemented the like service that will allow users to like and unlike post using firebase transaction blocks.

---
+++ Makestagram/Services/LikeService.swift
@@ -0,0 +1,89 @@
+//
+//  LikeService.swift
+//  Makestagram
+//
+//  Created by Shawn Miller on 7/13/17.
+//  Copyright © 2017 Make School. All rights reserved.
+//
+
+import Foundation
+import FirebaseDatabase
+
+struct LikeService {
+    static func create(for post: Post, success: @escaping (Bool) -> Void) {
+        // 1
+        guard let key = post.key else {
+            return success(false)
+        }
+        
+        // 2
+        let currentUID = User.current.uid
+        
+        // 3 code to like a post
+        
+        //liking a post. The following code takes you to the path in the database that contains the field where we verify if a user likes a post or not and set the value to true to verify that there wasa like
+        let likesRef = Database.database().reference().child("postLikes").child(key).child(currentUID)
+        likesRef.setValue(true) { (error, _) in
+            if let error = error {
+                assertionFailure(error.localizedDescription)
+                return success(false)
+            }
+            
+            return success(true)
+        }
+            let likeCountRef = Database.database().reference().child("posts").child(post.poster.uid).child(key).child("like_count")
+            likeCountRef.runTransactionBlock({ (mutableData) -> TransactionResult in
+                let currentCount = mutableData.value as? Int ?? 0
+                
+                mutableData.value = currentCount + 1
+                
+                return TransactionResult.success(withValue: mutableData)
+            }, andCompletionBlock: { (error, _, _) in
+                if let error = error {
+                    assertionFailure(error.localizedDescription)
+                    success(false)
+                } else {
+                    success(true)
+                }
+            })
+        }
+        
+
+    
+    // unliking a post. The following code takes you to the path in the database that contains the field where we verify if a user likes a post or not and set the value to nil to remove the existence of that like from the tree
+    static func delete(for post: Post, success: @escaping (Bool) -> Void) {
+        guard let key = post.key else {
+            return success(false)
+        }
+        
+        let currentUID = User.current.uid
+        
+        let likesRef = Database.database().reference().child("postLikes").child(key).child(currentUID)
+        likesRef.setValue(nil) { (error, _) in
+            if let error = error {
+                assertionFailure(error.localizedDescription)
+                return success(false)
+            }
+            
+            return success(true)
+        }
+        
+        let likeCountRef = Database.database().reference().child("posts").child(post.poster.uid).child(key).child("like_count")
+        likeCountRef.runTransactionBlock({ (mutableData) -> TransactionResult in
+            let currentCount = mutableData.value as? Int ?? 0
+            
+            mutableData.value = currentCount - 1
+            
+            return TransactionResult.success(withValue: mutableData)
+        }, andCompletionBlock: { (error, _, _) in
+            if let error = error {
+                assertionFailure(error.localizedDescription)
+                success(false)
+            } else {
+                success(true)
+            }
+        })
+        
+        
+    }
+}
